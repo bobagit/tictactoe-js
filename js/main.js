d0ce3edb6822815d521dbda647f47b2b8a2021ce
Robert Thompson <bob@jazzviolin.com>
2020-12-09 08:00:18 -0800

    Displaying on screen works

--- js/main.js
+++ js/main.js
@@ -1,42 +1,4 @@
-// TODO Cache DOM
-// TODO create gameBoard module
-const gameBoard = (function() {
-
-  // private
-  const tiles = [null, null, null, null, null, null, null, null, null];
-  const winningPattern = [
-    [0, 1, 2], [3, 4, 5], [6, 7, 8],
-    [0, 3, 6], [1, 4, 7], [2, 5, 8],
-    [0, 4, 8], [2, 4, 6]
-  ];
-  
-  checkWinner = (pieceId) => { 
-    for (let i = 0; i < winningPattern.length; i++) {
-      if (tiles[winningPattern[i][0]] == pieceId && 
-          tiles[winningPattern[i][1]] == pieceId && 
-          tiles[winningPattern[i][2]] == pieceId) {
-        console.log(`${pieceId.toUpperCase()} wins.`)
-      }   
-    }
-  }
-
-  // public
-  updateBoard = (position, pieceId) => {
-    tiles[position] = pieceId
-    checkWinner(pieceId)
-  }
-
-  return {
-    updateBoard
-  }
-})();
-
-const displayController = (function() {
-  const startMenu = document.getElementById('start-menu');
-  const board = document.getElementById('board');
-  // hide board
-  board.style.display = 'none'
-  
+const effects = (function () {
   const fadeOut = (element) => {
     var opacity = 1; // inital opacity is 100%
     function decrease() {
@@ -51,47 +13,97 @@ const displayController = (function() {
     }
     decrease();
   }
+  return {
+    fadeOut
+  }
+})();
 
-  // Select One or Two players
-  startMenu.addEventListener('click', e => {
-    if (e.target.classList.contains('one-player')) {
-      fadeOut(startMenu)
-      // #TODO make callback fn (so fadeOut doesn't collide with display none)
-      startMenu.style.display = 'none'
-      initiateBoard(1)
-
-      // fade out
-    } else if (e.target.classList.contains('two-players')) {
-      fadeOut(startMenu); // fade out
-      // #TODO make callback fn (so fadeOut doesn't collide with display none)
-      startMenu.style.display = 'none'
-      initiateBoard(2)
-    }
-  })
+const gameBoard = (function() {
+  // private
+  const tiles = [null, null, null, null, null, null, null, null, null];
+  const winningPattern = [
+    [0, 1, 2], [3, 4, 5], [6, 7, 8],
+    [0, 3, 6], [1, 4, 7], [2, 5, 8],
+    [0, 4, 8], [2, 4, 6]
+  ];
   
-
-
-  function initiateBoard(players) {
-    // game setup for two players
-    if (players == 2) {
-      board.style.display = 'flex'
-      for (let i = 0; i < 9; i++) { // nine tiles
-        let tile = document.createElement('div')
-        tile.classList.add('board-tile');
-        tile.setAttribute('data-id', i);
-        board.appendChild(tile);
-      }
-
+  checkWinner = (pieceId) => { 
+    for (let i = 0; i < winningPattern.length; i++) {
+      if (tiles[winningPattern[i][0]] == pieceId && 
+        tiles[winningPattern[i][1]] == pieceId && 
+        tiles[winningPattern[i][2]] == pieceId) {
+          console.log(`${pieceId.toUpperCase()} wins.`)
+      }   
     }
   }
 
+  // Public
+  updateBoard = (position, pieceId) => {
+    // update array
+    tiles[position] = pieceId
+    // update screen
+    let screenPosition = document.getElementById(position)
+    screenPosition.innerText = pieceId
+    checkWinner(pieceId)
+  }
+
+  return {
+    updateBoard
+  }
 })();
 
+const displayController = (function() {
+  const startMenu = document.getElementById('start-menu');
+  const board = document.getElementById('board');
+  // Hide board for start menu
+  board.style.display = 'none'
+
+  // Select number of players
+  startMenu.addEventListener('click', e => {
+    if (e.target.classList.contains('one-player')) {
+      effects.fadeOut(startMenu)
+      // #TODO make callback fn (so fadeOut doesn't collide with display none)
+      startMenu.style.display = 'none'
+      initiateBoard()
+
+    } else if (e.target.classList.contains('two-players')) {
+      effects.fadeOut(startMenu); // fade out
+      // #TODO make callback fn (so fadeOut doesn't collide with display none)
+      startMenu.style.display = 'none'
+      initiateBoard()
+    }
+
+    // #TODO
+    // setup setter for player turn to keep track of when player plays 
+    // setTurn? setter and getter?
+
+    board.addEventListener('click', e => {
+      e.preventDefault()
+      let tileId = e.target.getAttribute('data-id')
+      // match up the div data-id with the gameBoard.tile array
+      player1.play(tileId)
+    })
+
+  })
+
+  function initiateBoard(players) {
+    board.style.display = 'grid'
+    for (let i = 0; i < 9; i++) { // nine tiles
+      let tile = document.createElement('div')
+      tile.classList.add('board-tile');
+      tile.setAttribute('data-id', i);
+      tile.setAttribute('id', i);
+      board.appendChild(tile);
+    }
+  }  
+
+})();
+
+// Factory function
 const Player = (name, pieceId) => {
   const play = (position) => {
     gameBoard.updateBoard(position, pieceId)  
   }
-
   return {
     play,
     name,
@@ -101,10 +113,11 @@ const Player = (name, pieceId) => {
 
 let player1 = Player("Bob", "x")
 let player2 = Player("CPU", "o")
+
 // 0   1   2
 // 3   4   5
 // 6   7   8
-player1.play(0) // winner
-player1.play(1) // winner
-player1.play(2) // winner
+//player1.play(0) // winner
+//player1.play(1) // winner
+//player1.play(2) // winner
 
